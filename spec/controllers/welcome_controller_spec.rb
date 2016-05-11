771693a0938624de54cb94e680c877b01506c3db
mconiaris <mconiaris@gmail.com>
2016-05-11 15:21:30 -0400

    Created a test for a contact route in welcome controlller.

--- spec/controllers/welcome_controller_spec.rb
+++ spec/controllers/welcome_controller_spec.rb
@@ -21,4 +21,11 @@ RSpec.describe WelcomeController, type: :controller do
     end
   end
 
+  describe "GET #contact" do
+    it "renders the contact template" do
+      get :contact
+      expect(response).to render_template("contact")
+    end
+  end
+
 end
