0039ae2c6b6ab4fbc91fcc8c5871bb1d1cd08182
mconiaris <mconiaris@gmail.com>
2016-05-14 17:37:28 -0400

    Created a test for the index template.

--- spec/views/welcome/index.html.erb_spec.rb
+++ spec/views/welcome/index.html.erb_spec.rb
@@ -2,4 +2,11 @@ require 'rails_helper'
 
 RSpec.describe "welcome/index.html.erb", type: :view do
   pending "add some examples to (or delete) #{__FILE__}"
+
+  it "has text with \'Web Development\' on the page" do
+    render :template => "welcome/index.html.erb"
+
+    expect(rendered).to include "Web Development"
+  end
 end
+
