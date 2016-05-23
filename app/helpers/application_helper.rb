40864794856b1d76a24d2e29e65c8cbffc4b16ff
mconiaris <mconiaris@gmail.com>
2016-05-23 11:25:32 -0400

    Created an application helper method for meta descriptions.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,9 @@
 module ApplicationHelper
 
+  def description(meta_description)
+    content_for (:description) {meta_description}
+  end
+
   def render_title(page_title)
     content_for (:title) { page_title.to_s}
   end
